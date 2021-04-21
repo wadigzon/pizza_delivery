d7c9de5517248631a2b4dfba7d01d63bc3a4a352
WADIGZON DIAZ-WONG <wadigzon@users.noreply.github.com>
2021-04-21 15:45:50 -0700

    .- added basic libraries .- reading test file is working .- ready to start implementing solution

--- part2/main.ts
+++ part2/main.ts
@@ -1,1 +1,7 @@
 // part 2 of pizza deliverable problem
+import * as fs from 'fs';
+import * as path from 'path';
+
+const file = fs.readFileSync(path.join(__dirname + '/test/', 'PizzaDeliveryInput.txt'), 'utf8');
+const string = file.toString();
+console.log(string.length);
